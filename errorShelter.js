99c9bcdee56c22f99e2525c975fb8956aa77ca2b
Cory Robinson <coryrobinson42@gmail.com>
2015-07-29 22:07:47 -0700

    initial file

---
+++ errorShelter.js
@@ -0,0 +1,26 @@
+// eventSky.js - Event Aggregate utility/tool for front-end apps
+// https://github.com/crobinson42/event-sky
+// Cory Robinson
+
+(function () {
+
+	var init = function () {
+		alert('Error Shelter Initialized!');
+		console.dir(this);
+	};
+
+
+	var generateTime = function () {
+		return new Date().getTime();
+	};
+
+
+	// Exposed methods
+	window.errorShelter = {
+
+	};
+})();
+
+
+
+
